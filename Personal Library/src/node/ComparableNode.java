ca87fc372caebeb77ff56dbc71de047eec5198b0
Ozaren <krishna.sd.2012@gmail.com>
2016-04-05 09:37:33 -0400

    Custom Comparable Node
    made a customizable Comparable Node

---
+++ Personal Library/src/node/ComparableNode.java
@@ -0,0 +1,22 @@
+package node;
+
+public abstract class ComparableNode<V , E> extends ValueNode<V , E> {
+	public static boolean doAsending = true;
+	
+	abstract int getComparableValue();
+	
+	@Override
+	public final int compareTo(Node<V> compare) {
+		try {
+			@SuppressWarnings("unchecked")
+			ComparableNode<V , E> o = (ComparableNode<V , E>) compare;
+			if(doAsending)
+				return getComparableValue() - o.getComparableValue();
+			else
+				return o.getComparableValue() - getComparableValue();
+		}
+		catch(Exception e) {
+			return super.compareTo(compare);
+		}
+	}
+}
